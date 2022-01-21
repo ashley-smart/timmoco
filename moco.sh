70bb7c5d40cc20d4ae199b6a1ac82324cf440cf5
ashley-smart <40184749+ashley-smart@users.noreply.github.com>
2022-01-21 14:03:13 -0800

    Add files via upload

---
+++ moco.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+#SBATCH --mail-type=END,FAIL
+#SBATCH --job-name=moco_tac
+#SBATCH --partition=trc
+#SBATCH --time=96:00:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --output=/home/users/currier/job_outputs/%x.%j.out
+#SBATCH --open-mode=append
+
+module use /home/groups/trc/modules
+module load antspy/0.2.2
+
+python3 /home/users/currier/tac_analysis/scripts/motion_correction.py /oak/stanford/groups/trc/data/Tim/ImagingData/processed/20210824/TSeries-20210824-001
+
+python3 /home/users/currier/tac_analysis/scripts/motion_correction.py /oak/stanford/groups/trc/data/Tim/ImagingData/processed/20210824/TSeries-20210824-002
---
+++ motion_correction.py
@@ -0,0 +1,36 @@
+"""
+Motion correction script modified by TAC, original author MHT
+
+For use with single-channel volumetric timeseries (i.e GCaMP only)
+"""
+import sys
+import time
+import nibabel as nib
+import numpy as np
+from visanalysis.util import registration
+
+t0 = time.time()
+
+# first arg: path to image series base, without .suffix
+#   e.g. /oak/stanford/groups/trc/data/Max/ImagingData/Bruker/20210611/TSeries-20210611-001
+file_base_path = sys.argv[1]
+print('Registering brain file from {}'.format(file_base_path))
+
+# Load metadata from bruker .xml file
+metadata = registration.get_bruker_metadata(file_base_path + '.xml')
+print('Loaded metadata from {}'.format(file_base_path + '.xml'))
+
+# Load brain images
+ch1 = registration.get_ants_brain(file_base_path + '_channel_1.nii', metadata, channel=0)
+print('Loaded {}, shape={}'.format(file_base_path + '_channel_1.nii', ch1.shape))
+
+# Register channel 1 to reference image drawn from first x frames
+merged = registration.registerOneChannelToSelf(ch1, spatial_dims=len(ch1.shape) - 1, reference_frames=20)
+
+# Save registered, merged .nii
+nifti1_limit = (2**16 / 2)
+if np.any(np.array(merged.shape) >= nifti1_limit): # Need to save as nifti2
+    nib.save(nib.Nifti2Image(merged, np.eye(4)), file_base_path + '_reg.nii')
+else: # Nifti1 is OK
+    nib.save(nib.Nifti1Image(merged, np.eye(4)), file_base_path + '_reg.nii')
+print('Saved registered brain to {}. Total time = {:.1f}'.format(file_base_path + '_reg.nii', time.time()-t0))
